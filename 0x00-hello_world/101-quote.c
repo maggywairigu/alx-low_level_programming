bf137ede58f8344cd4b3b2cf49cb47a4c425cca0
maggywairigu <wairigumaina2016@gmail.com>
2023-02-16 17:12:04 -0800

    File 8 update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
-
+/**
+ * main - main block
+ * Return: 1
+ */
 int main(void)
 {
-  const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, str, 59);
-  return 1;
+    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, str, 59);
+    return (1);
 }
