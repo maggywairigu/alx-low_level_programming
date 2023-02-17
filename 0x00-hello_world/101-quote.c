11aa78357692f731c385574855298ea29004b0b0
maggywairigu <wairigumaina2016@gmail.com>
2023-02-16 17:14:25 -0800

    File 8 update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,10 @@
  */
 int main(void)
 {
-    const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, 59);
-    return (1);
+	int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+
+	write(2,
+	      "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      s);
+	return (1);
 }
