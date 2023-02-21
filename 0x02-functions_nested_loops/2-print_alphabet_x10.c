d425ce008356dcec292bde94733f7821f25f722d
maggywairigu <wairigumaina2016@gmail.com>
2023-02-21 10:25:53 -0800

    File 2 update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ *Return - 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter;
+
+	while (i < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
