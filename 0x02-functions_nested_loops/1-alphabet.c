625dd4cc56e00e34161ea5b5606ec58f21fdc8a6
maggywairigu <wairigumaina2016@gmail.com>
2023-02-21 09:43:08 -0800

    File 1 update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_alphabet -print the alphabet in lowercase
+ *Return: The alphabet in lowercase
+ *main - entry point
+ *Return: 0 Always
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
