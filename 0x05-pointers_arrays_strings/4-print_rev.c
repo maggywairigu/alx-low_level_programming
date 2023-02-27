08806009d91a999fd81a5a9d820e15cba93b81aa
maggywairigu <wairigumaina2016@gmail.com>
2023-02-27 09:14:31 -0800

    file 4 update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_rev - print a string value in reverse
+ *@s: pointer with the string values address
+ *Description: prints the value of *s in reverse
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] == '\0')
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
