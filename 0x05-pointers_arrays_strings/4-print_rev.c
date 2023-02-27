7c1e3bae4fb19710a63726b78f16cf9e609cfe3a
maggywairigu <wairigumaina2016@gmail.com>
2023-02-27 09:36:48 -0800

    file 4 bug fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@ void print_rev(char *s)
 {
 	int i;
 
-	i = 0;
-	while (s[i] == '\0')
-	{
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; i >= 0; i--)
 		_putchar(s[i]);
-		i--;
-	}
+
 	_putchar('\n');
 }
