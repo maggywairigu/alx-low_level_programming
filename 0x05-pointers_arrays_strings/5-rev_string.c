70b96c7164c4700d44cdab740f053fddb1a92e81
maggywairigu <wairigumaina2016@gmail.com>
2023-02-27 10:25:40 -0800

    file 6 update

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *rev_string - reverses a string
+ *@s: the string to be reversed
+ *Description: Reverses the string from pointer s
+ */
+void rev_string(char *s)
+{
+	int i,j,l;
+	char k
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+
+	for (i--; j = 0; j < l / 2; i--; j++)
+	{
+		k = s[j];
+		s[j] = s[i];
+		s[i] = k;
+	}
+}
