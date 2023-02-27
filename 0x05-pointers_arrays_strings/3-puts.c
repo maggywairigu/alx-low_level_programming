5d9ac9015ee713762c5381a979bfb04f6eb9702c
maggywairigu <wairigumaina2016@gmail.com>
2023-02-27 08:54:45 -0800

    file 3 update

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_puts - prints a value of a variable
+ *@str: pointer with the string value to be printed
+ *Description: prints out the value of*str
+ */
+void _puts(char *str)
+{
+	_putchar(*str);
+}
