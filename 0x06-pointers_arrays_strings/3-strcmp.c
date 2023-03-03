cad4f0c55fc4e3a42ddd54f382b2ec3daf12d16c
maggywairigu <wairigumaina2016@gmail.com>
2023-03-03 01:38:28 -0800

    file 3 update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strcmp - Compares two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: negative int if s1 < s2, 0 if matching and positive int otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
