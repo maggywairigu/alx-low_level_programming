056accac28bfad84c08c3813ce7c5d1fc6bfb582
maggywairigu <wairigumaina2016@gmail.com>
2023-03-10 09:58:18 -0800

    file 0 update

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - prints the program name
+ *@srgc: argument count
+ *@argv: array of string of program name
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	_putchar(*argv[0]);
+	return (0);
+}
