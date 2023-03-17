0aa35de45134318d2eeee8329db0642962361e9e
maggywairigu <wairigumaina2016@gmail.com>
2023-03-17 09:03:15 -0700

    file 4 update

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+  void *new_ptr;
+  unsigned int i;
+  if (new_size == 0) {
+    free(ptr);
+    return NULL;
+  }
+
+  if (ptr == NULL) {
+    return malloc(new_size);
+  }
+
+  if (new_size <= old_size) {
+    return ptr;
+  }
+
+  new_ptr = malloc(new_size);
+
+  if (new_ptr == NULL) {
+    return NULL;
+  }
+
+  for (i = 0; i < old_size; i++) {
+    *((char *)new_ptr + i) = *((char *)ptr + i);
+  }
+
+  free(ptr);
+
+  return new_ptr;
+}
