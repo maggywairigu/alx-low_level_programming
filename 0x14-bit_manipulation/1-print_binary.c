fceb2781a657765e151011056b4245b5697c6594
maggywairigu <wairigumaina2016@gmail.com>
2023-04-06 10:56:24 -0700

    file 1 update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - print binary representation of a number
+ * @n: decimal number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp = n;
+	int shifts = 0;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	while ((temp >>= 1) > 0)
+	{
+		shifts++;
+	}
+
+	while (shifts >= 0)
+	{
+		if ((n >> shifts) & 1)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("0");
+		}
+		shifts--;
+	}
+}
