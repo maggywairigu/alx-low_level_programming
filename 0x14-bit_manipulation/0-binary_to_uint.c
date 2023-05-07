bc2646b2aedbef23523bba55a6b828dcbab75a2e
maggywairigu <wairigumaina2016@gmail.com>
2023-05-07 01:38:53 -0700

    betty bug fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,23 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-  unsigned int sum, power;
-  int i;
+	unsigned int sum, power;
+	int i;
 
-  if (b == NULL)
-    return (0);
+	if (b == NULL)
+		return (0);
 
-  for (i = 0; b[i]; i++)
-    {
-      if (b[i] != '0' && b[i] != '1')
-	return (0);
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
 
-  for (power = 1, sum = 0, i--; i >= 0; i--, power *= 2)
-    {
-      if (b[i] == '1')
-	sum += power;
-    }
+	for (power = 1, sum = 0, i--; i >= 0; i--, power *= 2)
+	{
+		if (b[i] == '1')
+			sum += power;
+	}
 
-  return (sum);
+	return (sum);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,26 +1,26 @@
 #include "main.h"
 /**
  * print_binary - prints the binary representation of a number
- * @n: decimal number 
+ * @n: decimal number
  */
 void print_binary(unsigned long int n)
 {
-  unsigned long int temp;
-  int shifts;
+	unsigned long int temp;
+	int shifts;
 
-  if (n == 0)
-    {
-      _putchar('0');
-    }
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 
-  for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-    ;
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
 
-  for (; shifts >= 0; shifts--)
-    {
-      if ((n >> shifts) & 1)
-	_putchar('1');
-      else
-	_putchar('0');
-    }
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+	}
 }
